1a572a502aa67a233d33ea5c4fc47146bdb3f456
Brandon Bachrach <brandon.bachrach@gmail.com>
2018-12-10 16:00:47 -0500

    Fix logo

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <header>
 	<div id="logo">
-		<img src="media/SchwamWhiteShirtDesign.jpg">
+		<img src="assets/media/SchwamWhiteShirtDesign.jpg">
 	</div>
 </header>
     <form action="./form_submit.php" method="POST">
