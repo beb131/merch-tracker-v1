da8729b012058db046892495bdf6cbd7dc9645b8
Brandon Bachrach <brandon.bachrach@gmail.com>
2018-12-12 12:14:31 -0500

    Noindex, Nofollow

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
 	<script src="assets/js/main.js"></script>
 	<link rel='shortcut icon' href='assets/media/SchwamWhiteShirtDesign.jpg' type='image/x-icon'/ >
+	<meta name="robots" content="noindex, nofollow">
 </head>
 
 <body>
