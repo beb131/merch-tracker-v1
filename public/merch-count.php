4055b0dadbefa36a649ffc89a483922c058e52ba
Brandon Bachrach <brandon.bachrach@gmail.com>
2019-05-19 23:30:01 -0400

    Merch Count Udpate

--- public/merch-count.php
+++ public/merch-count.php
@@ -4,48 +4,68 @@
 Black Shirts:
     Ladies:
         S - 5
-        M - 3
-        L - 0
-    Mens:
-        S - 4
-        M - 4
+        M - 11
         L - 4
-        XL - 3
-
-Dyed Shirts:
-    Ladies:
-        S - 0
-        M - 0
-        L - 7
-        XL - 2
     Mens:
-        S - 3
-        M - 1
-        L - 3
-        XL - 4
-        2XL - 1
+        S - 11
+        M - 9
+        L - 9
+        XL - 9
 
-Undyed Shirts:
-        S - 4
-        M - 2
+Undyed T-Shirts:
+    Ladies:
+        S - 15
+        M - 12
         L - 0
         XL - 0
     Mens:
-        S - 6
+        S - 14
+        M - 11(I)
+        L - 13
+        XL - 7
+        2XL - 0
+
+Dyed T-Shirts:
+    Ladies:
+        S - 5
         M - 5
-        L - 4
-        XL - 4
+        L - 7
+        XL - 1
+    Mens:
+        S - 6
+        M - 11
+        L - 10
+        XL - 1
         2XL - 4
 
+Undyed Crop Tops:
+    S - 5
+    M/L - 20
+
+Dyed Crop Tops:
+    S - 5 (II)
+    M/L - 5 (II)
+
+Undyed Tank Tops:
+    S - 10
+    M - 10
+    L - 10
+    XL - 10
+
+Dyed Tank Tops:
+    S - 0
+    M - 5
+    L - 5(II)
+    XL - 0
+
 Hats: 4
-Beanies: 7
+Beanies: 16
 
-OTS: 12
+OTS: 10
 
-TNBF: 24
+TNBF: 21
 
-Posters: 15
+Posters: 14
 
 
 */
-?>
