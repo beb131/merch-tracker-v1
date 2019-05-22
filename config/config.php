f6a8d0046e0c4afee4263c0af5ad3caa077fb5a8
Brandon Bachrach <brandon.bachrach@gmail.com>
2019-05-22 00:37:44 -0400

    config folder

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+//DB Connection
+$host = "localhost";
+$username = "schwam_merch_adm";
+$password = "9mR*dU?KNK8Q";
+$dbname = "schwam_merchandise";
+
+    // $username = "schwam_merch_usr"; //User with less permissions
+    // $password = "X4y3q&riaU1X"
